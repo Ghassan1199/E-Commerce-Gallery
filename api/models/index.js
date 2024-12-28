167d63cd89268ebe75f4b3a4fc1981731d1266a3
Ghassan <ghassanmah@gmail.com>
2024-12-28 23:51:30 +0300

    fixing the db connection and make it more usable

--- api/models/index.js
+++ api/models/index.js
@@ -4,11 +4,13 @@ const DB_PORT = process.env.DB_PORT;
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_NAME = process.env.DB_NAME;
 
-const dbURI = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const dbURI = process.env.DB_URI;
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(dbURI);
+        await mongoose.connect(dbURI,clientOptions);
         console.log('Connected to MongoDB successfully!');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
