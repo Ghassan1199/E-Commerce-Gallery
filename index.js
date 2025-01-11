76b4459b692cf4e693b7bcc8d8678128b50f5e92
Ghassan <ghassanmah@gmail.com>
2025-01-11 04:33:39 +0300

    adding speed insights

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const { injectSpeedInsights } = require('@vercel/speed-insights');
 const dotenv = require('dotenv');
 dotenv.config();
 const morgan = require('morgan');
 const {connectDB} = require('./api/models/index');
 const router = require('./api/routes/index');
+
+injectSpeedInsights();
 
 const app = express();
 
